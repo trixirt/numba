46434cd39dfa9d2e6fdf351e03a0e74244a11464
Tom Rix <trix@redhat.com>
2023-12-02 09:38:04 -0500

    numba setup lllvmlite

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ min_python_version = "3.9"
 max_python_version = "3.13"  # exclusive
 min_numpy_build_version = "1.11"
 min_numpy_run_version = "1.22"
-min_llvmlite_version = "0.42.0dev0"
+min_llvmlite_version = "0.41"
 max_llvmlite_version = "0.43"
 
 if sys.platform.startswith('linux'):
